1e9aef8b4fba3d1bbeb3c82dc0c8d8838ae1cd04
EMD0817 <118962361+EMD0817@users.noreply.github.com>
2024-11-20 14:59:34 +0900

    画像アップロード機能の改善とプレビュー表示の追加完了前、function handleImageUpload(event)の実装を大幅に変更する直前のコミット

--- web/script.js
+++ web/script.js
@@ -1,5 +1,9 @@
 const InputIMG = new Image();
 const OutputIMG = new Image();
+const containerClass = document.querySelector('.container');
+const imgPreviewClass = document.querySelector('.imgPreview');
+const footerClass = document.querySelector('.footer');
+const previewImageID = document.querySelector('#previewImage');
 
 document.querySelector('.upload-button').addEventListener('click', function () {
     document.getElementById('imageUpload').click();
@@ -23,11 +27,16 @@ function handleImageUpload(event) {
         InputIMG.src = e.target.result;
     };
 
-    if (inputIMGSucceeded) {
-        reader.readAsDataURL(file);
-        console.log('AAA')
-        const containerClass = document.querySelector('container');
-        containerClass.style.display = 'none';
-    }
+    reader.readAsDataURL(file);
 
+    OutputIMG.src = InputIMG.src;
+    previewImg(OutputIMG);
+
+    containerClass.style.display = 'none';
+    imgPreviewClass.style.display = 'flex';
+    footerClass.style.display = 'flex';
+}
+
+function previewImg(img) {
+    previewImageID.src = img.src;
 }
