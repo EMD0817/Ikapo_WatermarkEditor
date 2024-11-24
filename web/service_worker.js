ff89d84e3e4fbf43f05067df533c8f71b902f1d2
EMD0817 <118962361+EMD0817@users.noreply.github.com>
2024-11-24 21:00:01 +0900

    PWA実装完了

--- web/service_worker.js
+++ web/service_worker.js
@@ -1,27 +1,55 @@
-// キャッシュファイルの指定
-var CACHE_NAME = 'pwa-sample-caches';
-var urlsToCache = [
-	'emd0817.github.io/Ikapo_WatermarkEditor/web/',
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+    '/images/Icon@2x.png',
+    '/images/photo.svg',
+    '/images/download.svg',
+    '/images/stars.svg',
+    '/images/new.svg',
+    '/images/share.svg',
+    '/images/X - Logo.svg',
+    '/watermarks/W01.png',
+    '/watermarks/W02.png',
+    '/Watermarks_Icon/WI01.png',
+    '/Watermarks_Icon/WI02.png',
 ];
 
-// インストール処理
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
                 return cache.addAll(urlsToCache);
             })
     );
 });
 
-// リソースフェッチ時のキャッシュロード処理
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
     event.respondWith(
-        caches
-            .match(event.request)
-            .then(function(response) {
-                return response ? response : fetch(event.request);
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
             })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
